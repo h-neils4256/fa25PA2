2788806ea7524c8fda7e9b510e0c6659ed3c9c6e
Hudson <hneils4256@sdsu.edu>
2025-10-26 16:30:19 -0700

    worked on push() and pop() methods of heap.h file

--- heap.h
+++ heap.h
@@ -15,7 +15,7 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap();
+        //TODO: insert index at end of heap, restore order using upheap
         data[size] = idx;
         upheap(size, weightArr);
         size++;
